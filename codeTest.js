9033abb2d2125cd34792ec283a432556ef5773a5
zhao <zhao.hanb@husky.neu.edu>
2019-11-07 11:21:29 -0500

    FizzBuzz

--- codeTest.js
+++ codeTest.js
@@ -765,3 +765,18 @@ var person = function Person(name) {
 }
 
 console.log("con:", person.name);
+/////////////////////////////////////////////////////////////////////////////////Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5////////////////////////////////////////////
+/////////////Fizzbuzz////////////////////
+
+function FizzbuzzFuc() {
+    for (let i = 0; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log(i + "FizzBuzz");
+        } else if (i % 5 == 0) {
+            console.log(i + "Buzz");
+        } else if (i % 3 == 0) {
+            console.log(i + "Fizz");
+        }
+    }
+}
+console.log(FizzbuzzFuc());
